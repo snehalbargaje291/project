23217b44023713d2a58b2e1fc66862c3b95c4265
snehal <snehal.bargaje@291gmail.com>
2021-11-30 11:41:58 +0530

    firstcommit

---
+++ Js/javascript.js
@@ -0,0 +1,40 @@
+burger= document.querySelector('.burger')
+NavBar= document.querySelector('.nav-bar')
+NavList= document.querySelector('.nav-list')
+
+
+
+burger.addEventListener('click',()=>{
+    NavList.classList.toggle('v-class');
+    NavBar.classList.toggle('h-nav');
+})
+
+//form
+let signUpForm = document.querySelector('.sign-up-form')
+let logInForm = document.querySelector('.log-in-form')
+
+signUpForm.addEventListener('click', ()=> {
+	document.getElementById('id01').style.display='block'
+})
+logInForm.addEventListener('click', ()=> {
+	document.getElementById('id02').style.display='block'
+})
+
+//sign-up
+
+var modal = document.getElementById('id01');
+
+		window.onclick = function(event) {
+			if (event.target == modal) {
+				modal.style.display = "none";
+			}
+		}
+
+//log-in
+
+var modal = document.getElementById('id01');
+		window.onclick = function(event) {
+			if (event.target == modal) {
+				modal.style.display = "none";
+			}
+		}
