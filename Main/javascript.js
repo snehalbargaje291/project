fc4538294b645057023f4410ca9f64d9492ed011
snehal <snehal.bargaje@291gmail.com>
2021-12-08 10:04:05 +0530

    responsive footer not working

---
+++ Main/javascript.js
@@ -0,0 +1,24 @@
+//AVINASH'S RESPONSIVE HAMBUREGER MENU---START
+function navSlide() {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll(".nav-links li");
+    
+    burger.addEventListener("click", () => {
+        //Toggle Nav
+        nav.classList.toggle("nav-active");
+        
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ""
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 15 + 0.3}s`;
+            }
+        });
+        //Burger Animation
+        burger.classList.toggle("toggle");
+    });
+}
+navSlide();
+//AVINASH'S RESPONSIVE HAMBUREGER MENU---END
